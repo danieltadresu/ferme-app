549d2c50d9b6e86bb5767cb9543dd302fcb22e3e
danieltadresu <danieldev1999@gmail.com>
2021-04-24 18:09:43 -0400

    feature: add method http to get all users data from the backend

--- reactjs-frontend/src/App.js
+++ reactjs-frontend/src/App.js
@@ -1,23 +1,68 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [isVisible, setIsVisible] = useState(false);
+  const [allUsers, setAllUsers] = useState([]);
+
+  const orderHandler = (value) => {
+    setIsVisible(value);
+    const fetchUsers = new Promise((resolve) => {
+      fetch('https://localhost:5001/api/user/all')
+        .then(response => response.json())
+        .then((data) => {
+          resolve(data)
+        });
+    });
+
+    fetchUsers.then((responseData) => {
+      setAllUsers(responseData);
+    })
+    
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!isVisible && 
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            React <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            rel="noopener noreferrer"
+            onClick={() => orderHandler(true)}
+          >
+            Haz click acá para enviar solicitudes HTTP al servidor
+          </a>
+        </header>}
+        {isVisible && 
+        <header className="App-users">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-users-link"
+            rel="noopener noreferrer"
+            style={{margin: '1rem 0'}}
+            onClick={() => orderHandler(false)}
+          >
+            Volver atrás
+          </a>
+          {allUsers.length > 0 && allUsers.map((item) => (
+            <div
+              style={
+                {maxWidth: '900px', 
+                margin: '2rem 0',
+              }}
+            >
+              <p style={{margin: '0px', padding: '0px'}}>
+                {item.userId} {item.name}
+              </p>
+            </div>
+          ))}
+        </header>}
     </div>
   );
 }
