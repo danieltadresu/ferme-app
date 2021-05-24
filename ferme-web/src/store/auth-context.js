c7139c08a364dc09c54ee10443de8c5f1d341811
danieltadresu <danieldev1999@gmail.com>
2021-05-24 16:40:19 -0400

    feature: save person id in localStorage

--- ferme-web/src/store/auth-context.js
+++ ferme-web/src/store/auth-context.js
@@ -6,6 +6,7 @@ const AuthContext = React.createContext({
   token: '',
   isLoggedIn: false,
   roleAccess: '',
+  personId: undefined,
   userName: '',
   login: (token) => {},
   logout: () => {},
@@ -22,6 +23,7 @@ const retrieveStoredToken = () => {
   const storedToken = localStorage.getItem('token');
   const storedExpirationDate = localStorage.getItem('expirationTime');
   const storedRoleAccess = localStorage.getItem('roleAccess');
+  const storedPersonId = localStorage.getItem('personId');
   const storedUserName = localStorage.getItem('userName');
 
   const remainingTime = calculateRemainingTime(storedExpirationDate);
@@ -29,6 +31,7 @@ const retrieveStoredToken = () => {
     localStorage.removeItem('token');
     localStorage.removeItem('expirationTime');
     localStorage.removeItem('roleAccess');
+    localStorage.removeItem('personId');
     localStorage.removeItem('userName');
     return null;
   }
@@ -36,6 +39,7 @@ const retrieveStoredToken = () => {
     token: storedToken,
     duration: remainingTime,
     roleAccess: storedRoleAccess,
+    personId: storedPersonId,
     userName: storedUserName,
   };
 };
@@ -44,37 +48,44 @@ export const AuthContextProvider = (props) => {
   const tokenData = retrieveStoredToken();
   let initialToken;
   let initialRoleAccess;
+  let initialPersonId;
   let initialUserName;
   if (tokenData) {
     initialToken = tokenData.token;
     initialRoleAccess = tokenData.roleAccess;
+    initialPersonId = tokenData.personId;
     initialUserName = tokenData.userName;
   }
   const [token, setToken] = useState(initialToken);
   const [roleAccess, setRoleAccess] = useState(initialRoleAccess);
+  const [personId, setPersonId] = useState(initialPersonId);
   const [userName, setUserName] = useState(initialUserName);
   const userIsLoggedIn = !!token;
 
   const logoutHandler = useCallback(() => {
     setToken(null);
     setRoleAccess(null);
+    setPersonId(undefined);
     setUserName(null);
     localStorage.removeItem('token');
     localStorage.removeItem('expirationTime');
     localStorage.removeItem('roleAccess');
+    localStorage.removeItem('personId');
     localStorage.removeItem('userName');
     if (logoutTimer) {
       clearTimeout(logoutTimer);
     }
   }, []);
 
-  const loginHandler = (token, expirationTime, roleAccess, userName) => {
+  const loginHandler = (token, expirationTime, roleAccess, personId, userName) => {
     setToken(token);
     setRoleAccess(roleAccess);
+    setPersonId(personId);
     setUserName(userName);
     localStorage.setItem('token', token);
     localStorage.setItem('expirationTime', expirationTime);
     localStorage.setItem('roleAccess', roleAccess);
+    localStorage.setItem('personId', personId);
     localStorage.setItem('userName', userName);
     const remainingTime = calculateRemainingTime(expirationTime);
     logoutTimer = setTimeout(logoutHandler, remainingTime);
@@ -90,6 +101,7 @@ export const AuthContextProvider = (props) => {
     token: token,
     isLoggedIn: userIsLoggedIn,
     roleAccess,
+    personId,
     userName,
     login: loginHandler,
     logout: logoutHandler,
