e20043d43cbe501e39ca51a63272a748a2430614
danieltadresu <danieldev1999@gmail.com>
2021-06-01 01:36:58 -0400

    improvement: add table to list all users

--- ferme-web/src/components/Layout/Operations/UserList/UserList.js
+++ ferme-web/src/components/Layout/Operations/UserList/UserList.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
-import { Row, Col, Button, Card, Table, Tag, Space, Skeleton } from 'antd';
+import { Row, Col, Button, Card, Table, Tag, Space, Skeleton, Select } from 'antd';
 import 'antd/dist/antd.css';
 import classes from './UserList.module.css'
 import {
@@ -18,6 +18,7 @@ const UserList = (props) => {
     axios
     .get('https://localhost:5001/api/user/roles/all')
     .then((response) => {
+      console.log('response :>> ', response);
       setUsers(response.data);
       setIsLoading(false);
     });
@@ -42,6 +43,49 @@ const UserList = (props) => {
       dataIndex: 'id',
       key: 'id',
     },
+    {
+      title: <span style={{ fontSize: '.7rem' }}>Rol</span>,
+      dataIndex: 'userRole',
+      key: 'userRole',
+      render: (userRole) => (
+        <>
+          {userRole === 'ADMIN' && <Tag color="blue">ADMNISTRADOR</Tag>}
+          {userRole === 'PROVIDER' && <Tag color="purple">PROVEEDOR</Tag>}
+          {userRole === 'EMPLOYEE' && <Tag color="volcano">EMPLEADO</Tag>}
+          {userRole === 'CUSTOMER' && <Tag color="red">CLIENTE PERSONA NATURAL</Tag>}
+          {userRole === 'COMPANY' && <Tag color="magenta">CLIENTE EMPRESA</Tag>}
+        </>
+      ),
+    },
+    {
+      title: <span style={{ fontSize: '.7rem' }}>Email</span>,
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: <span style={{ fontSize: '.7rem' }}>Nombre</span>,
+      dataIndex: 'userFullName',
+      key: 'userFullName',
+    },
+    {
+      dataIndex: 'id',
+      key: 'id',
+      render: (id) => (
+        <>
+          <Space size="middle">
+            <a
+              style={{
+                color: '#0918EB',
+                cursor: 'pointer',
+                fontSize: '.8rem'
+              }}
+            >
+              <FileSearchOutlined /> Ver Detalle
+            </a>
+          </Space>
+        </>
+      )
+    }
   ];
 
   return (
