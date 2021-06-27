c41f59d1428168731ef50bf911120e45c2154c2e
danieltadresu <danieldev1999@gmail.com>
2021-06-27 15:47:45 -0400

    feature: Integrate endpoints to get, read and save Product Order Data

--- ferme-web/src/components/Layout/Purchase/PurchaseStripeIntegration/PurchaseStripeIntegration.js
+++ ferme-web/src/components/Layout/Purchase/PurchaseStripeIntegration/PurchaseStripeIntegration.js
@@ -1,74 +1,42 @@
-import { useContext, useEffect, useState } from 'react';
-import AuthContext from '../../../../store/auth-context';
-import PropTypes from 'prop-types';
-import { Result, Button } from 'antd';
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "../../../../store/auth-context";
+import PropTypes from "prop-types";
+import { Result, Button } from "antd";
 import "antd/dist/antd.css";
-import classes from './PurchaseStripeIntegration.module.css';
-import axios from 'axios';
+import classes from "./PurchaseStripeIntegration.module.css";
+import axios from "axios";
 import { loadStripe } from "@stripe/stripe-js";
-const stripePromise = loadStripe("pk_test_51IrBoLEskkhmHZqbEdjFy7Xfe1AdcJd1prD4btlivss9FlYQ216jNri7Ljxp9KJTddknOMMUKU8OEX5LTsMwbJbx00tWQtUVKM");
+const stripePromise = loadStripe(
+  "pk_test_51IrBoLEskkhmHZqbEdjFy7Xfe1AdcJd1prD4btlivss9FlYQ216jNri7Ljxp9KJTddknOMMUKU8OEX5LTsMwbJbx00tWQtUVKM"
+);
 
 const PurchaseStripeIntegration = (props) => {
-
-  const [ stripeToken, setStripeToken ] = useState();
+  const [stripeToken, setStripeToken] = useState();
   const authCtx = useContext(AuthContext);
 
   const purchaseHandler = async () => {
-    console.log('authCtx.userCart :>> ', authCtx.userCart);
-    // if (!props.cartProducts) {
-    //   return;
-    // }
-    console.log('props.delivery :>> ', props.delivery);
-    // const data = [
-    //   {
-    //     "Id": 2,
-    //     "ProductId": 1,
-    //     "ProductQuantity": 12
-    //   },
-    //   {
-    //     "Id": 1,
-    //     "ProductId": 1,
-    //     "'ProductQuantity": 1    
-    //   }
-    // ]
-    console.log('authCtx.userCart :>> ', authCtx.userCart);
     const data = authCtx.userCart.map((cart) => {
       return {
-        "Id": cart.id,
-        "ProductId": cart.id,
-        "ProductQuantity": cart.quantity,
-        "ProductName": cart.name,
-        "ProductUnitPrice": cart.unitPrice,
-      }
-    })
-    const totalPurchase = authCtx.userCart.map((item) => item.itemPrice).reduce((a, b) => a + b, 0);
-    axios.post(`https://localhost:5001/api/order/${authCtx.personId}/${props.delivery.isHomeDelivery ? 1 : 2}/${totalPurchase}`, data).then((response) => {
-      console.log('response :>> ', response);
-      setStripeToken(response.data);
+        Id: cart.id,
+        ProductId: cart.id,
+        ProductQuantity: cart.quantity,
+        ProductName: cart.name,
+        ProductUnitPrice: cart.unitPrice,
+      };
     });
-    // const test = async () => {
-    //   axios.post("https://localhost:5001/api/order", data);
-    // };
-    // const newCustomerPurchase = {
-    //   id: 1,
-    //   productQuantity: props.cartProducts.productQuantity ? props.cartProducts.productQuantity : 1,
-    //   totalPurchase: props.cartProducts.price,
-    //   paymentMethodId: 1,
-    //   deliveryTypeId: props.delivery.isHomeDelivery ? 1 : 2,
-    //   customerId: authCtx.personId,
-    //   productId: props.cartProducts.id,
-    //   createdat: 0,
-    //   updatedat: 0,
-    // };
-    // console.log('newCustomerPurchase :>> ', newCustomerPurchase);
-    // console.log('newCustomerPurchase :>> ', props.cartProducts);
-    // axios.post(
-    //   `https://localhost:5001/api/customerpurchase/session/${props.cartProducts.id}`, 
-    //   newCustomerPurchase
-    // )
-    // .then((response) => {
-    //   setStripeToken(response.data);
-    // })
+    const totalPurchase = authCtx.userCart
+      .map((item) => item.itemPrice)
+      .reduce((a, b) => a + b, 0);
+    const personId = authCtx.personId;
+    const deliveryTypeId = props.delivery.isHomeDelivery ? 1 : 2;
+    axios
+      .post(
+        `https://localhost:5001/api/order/${personId}/${deliveryTypeId}/${totalPurchase}`,
+        data
+      )
+      .then((response) => {
+        setStripeToken(response.data);
+      });
   };
 
   const purchaseStripeHandler = async () => {
@@ -80,7 +48,7 @@ const PurchaseStripeIntegration = (props) => {
 
   useEffect(() => {
     if (stripeToken) {
-      purchaseStripeHandler(); 
+      purchaseStripeHandler();
     }
   }, [stripeToken]);
 
@@ -90,15 +58,12 @@ const PurchaseStripeIntegration = (props) => {
         title="A continuación serás redirigido hacia un sitio externo donde podrás realizar tu pago"
         subTitle="El sistema de pago se encuentra protegido y podrás realizar la operación con tranquilidad."
         extra={
-          <Button
-            type="primary"
-            key="console"
-            onClick={purchaseHandler}
-          >
+          <Button type="primary" key="console" onClick={purchaseHandler}>
             Ir al sitio
           </Button>
         }
-      />,
+      />
+      ,
     </div>
   );
 };
--- ferme-web/src/pages/Operations.js
+++ ferme-web/src/pages/Operations.js
@@ -7,6 +7,7 @@ import BillList from '../components/Layout/Operations/BillList/BillList';
 import AddUser from '../components/Layout/Operations/AddUser/AddUser';
 import UserList from '../components/Layout/Operations/UserList/UserList';
 import AddOrder from '../components/Layout/Operations/AddOrder/AddOrder';
+import OrderList from '../components/Layout/Operations/OrderList/OrderList';
 import 'antd/dist/antd.css';
 const Operations = () => {
   const [selectedItem, setSelectedItem] = useState(0);
@@ -24,6 +25,7 @@ const Operations = () => {
       {selectedItem === 2 && <ProductDetail />}
       {selectedItem === 4 && <BillList selectedItem={itemChangeHandler} />}
       {selectedItem === 5 && <AddOrder selectedItem={itemChangeHandler} />}
+      {selectedItem === 6 && <OrderList selectedItem={itemChangeHandler} />}
       {selectedItem === 7 && <AddUser selectedItem={itemChangeHandler} /> }
       {selectedItem === 8 && <UserList selectedItem={itemChangeHandler} /> }
     </>
